2bf44ed715fd596da220081496ab63d8e33cbde9
KAZUMA Ukyo <yaakaito@gmail.com>
2012-07-07 00:31:32 +0900

    init

---
+++ VerifyViewTipsTests/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VerifyViewTipsTests
+//
+//  Created by KAZUMA Ukyo on 12/07/07.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GHUnit.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIPhoneAppDelegate");
+    }
+}
